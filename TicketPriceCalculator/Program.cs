90dd902e2997bb1ecf655ee0d1a3b25ccfe661be
hannah-buer <103133203+hannah-buer@users.noreply.github.com>
2024-06-02 21:28:35 +0000

    Ticket Price Calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿int age;
+int PriceOfTicket = 10;
+int NewPriceOfTicket = 7;
+while (true)
+{
+    Console.WriteLine("Enter age: ");
+    
+    if(!int.TryParse(Console.ReadLine(),out age))
+    {
+        Console.WriteLine("Please enter a valid integer. ");
+        continue;
+    }
+    if (age < 0)
+    {
+        Console.WriteLine("Please enter a valid age.");
+    }
+    if (age >= 0){
+        if (age > 64 || age < 13)
+        {
+            Console.WriteLine("Amount due: " + NewPriceOfTicket+"GHC");
+        }
+        else{            
+            Console.WriteLine("Amount due: " + PriceOfTicket+ "GHC");
+        }
+        break;
+    }
+}
+
+
+Thread.Sleep(5000);
+Environment.Exit(0);
